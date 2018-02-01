c9dee89d2bd3ca970d0c9c6b394971eca6d47dbd
Neo Chang <me@neoto.xin>
2018-02-01 13:21:25 +0800

    Using Vue to do DOM

--- src/map.js
+++ src/map.js
@@ -1,42 +1,10 @@
-import './map.css'
-import * as d3 from 'd3'
-import { tubeMap } from 'd3-tube-map'
+import App from '@/map/App'
+import Vue from 'vue'
+Vue.config.productionTip = false
 
-var container = d3.select('#map-container')
-
-var width = 1200
-var height = 1200
-
-var map = tubeMap()
-.width(width)
-.height(height)
-.margin({
-  top: height / 50,
-  right: width / 7,
-  bottom: height / 10,
-  left: width / 7
-})
-.on('click', name => {
-  console.log(name)
-})
-d3.json('/static/pubs.json', (error, data) => {
-  console.log(error)
-  container.datum(data).call(map)
-  var svg = container.select('svg')
-
-  var zoom = d3
-    .zoom()
-    .scaleExtent([0.5, 6])
-    .on('zoom', zoomed)
-
-  var zoomContainer = svg.call(zoom)
-  var initialScale = 1
-  var initialTranslate = [100, 200]
-
-  zoom.scaleTo(zoomContainer, initialScale)
-  zoom.translateTo(zoomContainer, initialTranslate[0], initialTranslate[1])
-
-  function zoomed () {
-    svg.select('g').attr('transform', d3.event.transform.toString())
-  }
+/* eslint-disable no-new */
+new Vue({
+  el: '#map-container',
+  template: '<App/>',
+  components: { App }
 })
---
+++ src/map/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <svg v-if="map" style="width: 100%; height: 100%;">
+    <g>
+      <path
+        :d="drawRiver(map.rivers)"
+        stroke="#CCECF4"
+        fill='none'
+        :stroke-width="1.8*map.lineWidth"
+      />
+    </g>
+    <g>
+      <path
+      v-for="line in map.lines"
+      :d="drawRiver(line)"
+      :stroke="line.color"
+      fill="none"
+      :stroke-width="line.highlighted ? map.lineWidth * 1.3 : map.lineWidth"
+      />
+    </g>
+  </svg>
+  <div v-else>
+    Loading
+  </div>
+</template>
+
+<script>
+import Leaflet from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { line } from './curve'
+import axios from 'axios'
+import MapData from './data'
+export default {
+  data () {
+    return {
+      map: null
+    }
+  },
+  mounted () {
+    axios.get('/static/pubs.json')
+    .then(response => {
+      console.log(response.data)
+      this.map = new MapData(response.data)
+    })
+  },
+  methods: {
+    drawRiver (data) {
+      return line(data, this.map.xScale, this.map.yScale, this.map.lineWidth, this.map.lineWidthTickRatio)
+    }
+  },
+  components: {
+    'v-map': Leaflet.Map,
+    'v-tilelayer': Leaflet.TileLayer,
+    'v-marker': Leaflet.Marker
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+}
+#map-container {
+  width:100%;
+  height:100%;
+}
+
+</style>
