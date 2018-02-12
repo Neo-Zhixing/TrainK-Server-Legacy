c3fa3c9372a75781448225440fb306f60a879532
Neo Chang <me@neoto.xin>
2018-02-12 21:56:38 +0800

    Adding Favicon

--- TrainK/settings.py
+++ TrainK/settings.py
@@ -160,6 +160,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'dist'),
+    os.path.join(BASE_DIR, 'assets')
 )
 
 CELERY_WORKER_REDIRECT_STDOUTS = False
